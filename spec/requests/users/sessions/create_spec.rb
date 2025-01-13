98577c7ccf3f34975cd8e4c1c13257af7fecae55
Santiago Pais <142631625+santipais@users.noreply.github.com>
2025-01-13 14:58:53 -0300

    New me endpoint and added the missing specs (#2)
    * New me endpoint and added the missing specs
    
    * fix pr comments
    
    * removed me action

---
+++ spec/requests/users/sessions/create_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v1/users/sign_in', type: :request do
+  let(:user) { create(:user) }
+  let(:email) { user.email }
+  let(:password) { user.password }
+  let(:json_response) { json }
+  let(:params) do
+    {
+      user: { email:, password: }
+    }
+  end
+
+  subject { post user_session_path, params:, as: :json }
+
+  context 'when the credentials are correct' do
+    it 'returns a successful response' do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns the user' do
+      subject
+      expect(json_response[:id]).to eq(user.id)
+      expect(json_response[:email]).to eq(user.email)
+      expect(json_response[:created_at]).to eq(User.last.created_at.to_s)
+    end
+
+    it 'returns a valid client and access token' do
+      subject
+      expect(response.header['Authorization']).to be_present
+    end
+  end
+
+  context 'when the credentials are incorrect' do
+    context 'when email is incorrect' do
+      let(:email) { 'incorrect@email.com' }
+
+      it 'returns an unauthorized response' do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns an error message' do
+        subject
+        expect(json_response[:error]).to eq('Invalid Email or password.')
+      end
+
+      it 'does not return a valid client and access token' do
+        subject
+        expect(response.header['Authorization']).to be_nil
+      end
+    end
+
+    context 'when password is incorrect' do
+      let(:password) { 'incorrect_password' }
+
+      it 'returns an unauthorized response' do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns an error message' do
+        subject
+        expect(json_response[:error]).to eq('Invalid Email or password.')
+      end
+
+      it 'does not return a valid client and access token' do
+        subject
+        expect(response.header['Authorization']).to be_nil
+      end
+    end
+  end
+end
---
+++ spec/requests/users/sessions/destroy_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'DELETE /api/v1/users/sign_out', type: :request do
+  let(:user) { create(:user) }
+  let(:headers) { auth_header(user) }
+  let(:json_response) { json }
+
+  subject { delete destroy_user_session_path, headers:, as: :json }
+
+  context 'when the user is signed in' do
+    it 'returns a successful response' do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns the user' do
+      subject
+      expect(json_response[:id]).not_to be_nil
+      expect(json_response[:email]).to eq(user.email)
+      expect(json_response[:created_at]).to eq(User.last.created_at.to_s)
+    end
+
+    it 'invalidates the existing JWT' do
+      expect { subject }.to(change { user.reload.jti })
+    end
+  end
+
+  context 'when the user is not signed in' do
+    let(:headers) { {} }
+
+    it 'returns an not found response' do
+      subject
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it 'returns an error message' do
+      subject
+      expect(json_response[:message]).to eq('No active session')
+    end
+  end
+end
