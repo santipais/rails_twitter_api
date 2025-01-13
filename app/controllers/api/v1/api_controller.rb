9cc7dbfeac5f88d4311eb3a4a755e70f9c7ab44e
Santiago Pais <santiago.pais@gogrow.dev>
2025-01-13 13:13:10 -0300

    removed me action

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,9 +3,6 @@
 module Api
   module V1
     class ApiController < Api::ApplicationController
-      def me
-        render json: UserSerializer.render(current_user, view: :me)
-      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,4 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'api/v1/users', defaults: { format: :json },
                      controllers: { sessions: 'api/v1/users/sessions', registrations: 'api/v1/users/registrations' }
-
-  namespace :api do
-    namespace :v1 do
-      get 'me' => 'api#me'
-    end
-  end
 end
