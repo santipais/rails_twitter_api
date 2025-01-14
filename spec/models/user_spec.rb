6d5079e48f173fd98daf68083a51a3a87dc1955e
Santiago Pais <santiago.pais@gogrow.dev>
2025-01-14 12:24:39 -0300

    fix user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe User, type: :model do
     subject { build(:user) }
 
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to allow_value('test@example.com').for(:email) }
-    it { is_expected.not_to allow_value('tes@t@example.com').for(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
     it { is_expected.to validate_presence_of(:password) }
@@ -17,9 +15,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_length_of(:username).is_at_least(2).is_at_most(20) }
     it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
-    it { is_expected.to allow_value('username').for(:username) }
-    it { is_expected.not_to allow_value('user name').for(:username) }
-    it { is_expected.not_to allow_value('invalid/username').for(:username) }
 
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_length_of(:first_name).is_at_least(2) }
@@ -29,22 +24,25 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_length_of(:bio).is_at_most(160) }
 
-    it { is_expected.to allow_value('https://www.example.com').for(:website) }
-    it { is_expected.to allow_value('http://www.example.com').for(:website) }
-    it { is_expected.to allow_value('https://example.com').for(:website) }
-    it { is_expected.to allow_value('http://example.com').for(:website) }
-    it { is_expected.not_to allow_value('www.example.com').for(:website) }
-    it { is_expected.not_to allow_value('example.com').for(:website) }
-
     it { is_expected.to validate_presence_of(:birthdate) }
+    it { is_expected.to allow_value(19.years.ago).for(:birthdate) }
+    it { is_expected.not_to allow_value(17.years.ago).for(:birthdate).with_message('must be less than or equal to 18 years ago') }
 
-    it 'validates that birthdate is at least 18 years ago' do
-      user = build(:user, birthdate: 17.years.ago.to_date)
-      expect(user).not_to be_valid
-      expect(user.errors[:birthdate]).to include('must be less than or equal to 18 years ago')
+    context 'validating regular expressions' do
+      it { is_expected.to allow_value('test@example.com').for(:email) }
+      it { is_expected.not_to allow_value('tes@t@example.com').for(:email) }
 
-      user.birthdate = 18.years.ago.to_date
-      expect(user).to be_valid
+      it { is_expected.to allow_value('https://www.example.com').for(:website) }
+      it { is_expected.to allow_value('http://www.example.com').for(:website) }
+      it { is_expected.to allow_value('https://example.com').for(:website) }
+      it { is_expected.to allow_value('http://example.com').for(:website) }
+      it { is_expected.not_to allow_value('www.example.com').for(:website) }
+      it { is_expected.not_to allow_value('http://exam:ple.com').for(:website) }
+      it { is_expected.not_to allow_value('example.com').for(:website) }
+
+      it { is_expected.to allow_value('username').for(:username) }
+      it { is_expected.not_to allow_value('user name').for(:username) }
+      it { is_expected.not_to allow_value('invalid/username').for(:username) }
     end
   end
 
