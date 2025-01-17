d739b291014f33124f90fba29ce742a0ab6fc7f0
Santiago Pais <santiago.pais@gogrow.dev>
2025-01-17 10:31:59 -0300

    Fix PR commets

--- spec/requests/api/v1/tweets/show_spec.rb
+++ spec/requests/api/v1/tweets/show_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'GET /api/v1/tweets/:id', type: :request do
 
   subject { get api_v1_tweet_path(tweet_id), headers:, as: :json }
 
-  context 'when the params are correct' do
-    context 'when the user is authenticated' do
+  context 'when the user is authenticated' do
+    context 'when the tweet_id is valid' do
       it 'returns a ok response' do
         subject
         expect(response).to have_http_status(:ok)
@@ -20,32 +20,16 @@ RSpec.describe 'GET /api/v1/tweets/:id', type: :request do
 
       it 'returns the tweet' do
         subject
-        expect(json_response[:id]).to eq(tweet.id)
-        expect(json_response[:content]).to eq(tweet.content)
+
+        expect(json_response.slice(:id, :content).as_json)
+          .to eq(tweet.slice(:id, :content))
         expect(json_response[:posted_ago]).to eq(time_ago_in_words(tweet.created_at))
-        expect(json_response[:user][:id]).to eq(tweet.user.id)
-        expect(json_response[:user][:username]).to eq(tweet.user.username)
-        expect(json_response[:user][:first_name]).to eq(tweet.user.first_name)
-        expect(json_response[:user][:last_name]).to eq(tweet.user.last_name)
+
+        expect(json_response[:user].slice(:id, :username, :first_name, :last_name).as_json)
+          .to eq(tweet.user.slice(:id, :username, :first_name, :last_name))
       end
     end
 
-    context 'when the user is not authenticated' do
-      let(:headers) { {} }
-
-      it 'returns an unauthorized response' do
-        subject
-        expect(response).to have_http_status(:unauthorized)
-      end
-
-      it 'returns an unauthorized message' do
-        subject
-        expect(json_response[:error]).to include('You need to sign in or sign up before continuing.')
-      end
-    end
-  end
-
-  context 'when the params are incorrect' do
     context 'when the tweet_id is invalid' do
       let(:tweet_id) { 'invalid_id' }
 
@@ -58,6 +42,20 @@ RSpec.describe 'GET /api/v1/tweets/:id', type: :request do
         subject
         expect(json_response[:error]).to include('Resource not found.')
       end
+    end
+  end
+
+  context 'when the user is not authenticated' do
+    let(:headers) { {} }
+
+    it 'returns an unauthorized response' do
+      subject
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'returns an unauthorized message' do
+      subject
+      expect(json_response[:error]).to include('You need to sign in or sign up before continuing.')
     end
   end
 end
